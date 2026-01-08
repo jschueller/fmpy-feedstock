d885a69dde9491f1193d5079dc86541dc5caab24
Julien Schueller <schueller@phimeca.com>
2026-01-08 09:46:23 +0100

    Update to 0.3.27

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -1,2 +1,7 @@
-for %%f in (*.whl) do %PYTHON% -m pip install --no-deps -vv "%%f"
-if errorlevel 1 exit 1
+cd native
+%PYTHON% download_binaries.py
+set "PYTHONPATH=%CWD%\..\src"
+%PYTHON% build_binaries.py
+
+%PYTHON% -m build --wheel --skip-dependency-check --no-isolation
+%PYTHON% -m installer dist\*.whl
--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-$PYTHON -m pip install --no-deps -vv *.whl
+cd native
+$PYTHON download_binaries.py
+PYTHONPATH=$PWD/../src $PYTHON build_binaries.py
+
+$PYTHON -m build --wheel --skip-dependency-check --no-isolation
+$PYTHON -m installer dist/*.whl
